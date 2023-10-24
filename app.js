e415955998dff943f1771ec816772fef9217bf85
ohseungmin <ohmin2886@gmail.com>
2023-10-24 19:58:05 +0900

    마지막 요청 쿼리스트링 방식으로 데이터 분석

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const queryString = require('querystring');
 
 // 요청과 응답 매개변수
 http.createServer((req,res) => {
@@ -7,6 +8,10 @@ http.createServer((req,res) => {
     // 새 문자열에 데이터 조각을 문자열로 변환한 것을 대입
     req.on('data', chunk => {
       body += chunk.toString()
+    })
+    // 선언 쿼리스트링 방식으로 데이터를 넣은 body를 분석 요청
+    req.on('end', () => {
+      const parsedBody = queryString.parse(body);
     })
   }
 })
